3848cb5a632ac5accb4c4980ce849be063b8a8d4
Alex Zdatny <alexzdatny@gmail.com>
2018-12-19 22:21:17 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+let driverId = 0;
+let passengerId = 0;
+let tripId = -;
+let store = {drivers: [], passengers: [], trips: []};
+
+class Driver {
+  constructor(name) {
+    this.id = ++driverId;
+    this.name = name;
+    store.drivers.push(this);
+
+    trips() {
+      return store.trips.filter(trip => {
+        return trip.DriverId === this.id;
+      })
+    }
+  }
+}
